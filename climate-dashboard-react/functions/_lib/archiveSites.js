b149d390f4c56fe15a39581ffe94cb8e75afe92c
Will Simons <willsimons@Wills-Laptop.local>
2026-09-19 17:27:30 +0100

    Changed name back

--- climate-dashboard-react/functions/_lib/archiveSites.js
+++ climate-dashboard-react/functions/_lib/archiveSites.js
@@ -10,7 +10,7 @@ export const ARCHIVE_SITES = {
       },
       {
         role: "partner",
-        name: "Will Simons",
+        name: "Ennia Bosshard",
         email: "hello@wildclocks.io",
         },
     ],
